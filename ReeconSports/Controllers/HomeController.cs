033f9902b13787770d3722420d5fef7254c8a9a1
Danilo Diniz <danilodiniz93@yahoo.com>
2023-10-29 11:38:17 -0300

    Starting project

---
+++ ReeconSports/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReeconSports.Models;
+using System.Diagnostics;
+
+namespace ReeconSports.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
